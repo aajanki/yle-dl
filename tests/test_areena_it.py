01e14a35a23c088975422178481d85c67cecedb0
Antti Ajanki <antti.ajanki@iki.fi>
2017-09-16 10:27:58 +0300

    Fix a test by changing to a non-expired program

--- tests/test_areena_it.py
+++ tests/test_areena_it.py
@@ -97,8 +97,7 @@ def test_areena_html5_clip_stream_url():
 
 
 def test_areena_episode_pages():
-    episodes = fetch_episode_pages('https://areena.yle.fi/1-3439855')
+    episodes = fetch_episode_pages('https://areena.yle.fi/1-3822119')
 
     # The first page contains 12 episodes, make sure we get several pages
-    assert len(episodes) > 50
-    assert all(u.startswith('https://areena.yle.fi/1-') for u in episodes)
+    assert len(episodes) > 20
