4606bf08e86496f8d2a89101be35c88a329c680f
Antti Ajanki <antti.ajanki@iki.fi>
2017-11-04 09:28:26 +0200

    Correct casing for a package name

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     packages=['yledl'],
     include_package_data=True,
     install_requires=[
-        'pycrypto', 'requests', 'lxml', 'pyamf'
+        'pycrypto', 'requests', 'lxml', 'PyAMF'
     ] + ssl_sni_requires,
     extras_require = {
         'youtubedl-backend': ['youtube_dl']
