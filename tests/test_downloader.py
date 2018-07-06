df146b4229390f41a247d0826a553e63c78ec2e0
Antti Ajanki <antti.ajanki@iki.fi>
2018-07-06 18:27:41 +0300

    Make the test state a local variable

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -11,30 +11,21 @@ from yledl import YleDlDownloader, Clip, FailedClip, BaseDownloader, \
 from utils import Capturing
 
 
-download_state = {'command': None, 'stream_id': None}
-
-
-def clear_state():
-    global download_state
-    download_state = {'command': None, 'stream_id': None}
-
-
 class StateCollectingBackend(BaseDownloader):
-    def __init__(self, id):
+    def __init__(self, state_dict, id):
         BaseDownloader.__init__(self, '.mp4')
         self.id = id
+        self.state_dict = state_dict
 
     def save_stream(self, clip_title, io):
-        global download_state
-        download_state['command'] = 'download'
-        download_state['stream_id'] = self.id
+        self.state_dict['command'] = 'download'
+        self.state_dict['stream_id'] = self.id
 
         return RD_SUCCESS
 
     def pipe(self, io, subtitle_url):
-        global download_state
-        download_state['command'] = 'pipe'
-        download_state['stream_id'] = self.id
+        self.state_dict['command'] = 'pipe'
+        self.state_dict['stream_id'] = self.id
 
         return RD_SUCCESS
 
@@ -46,8 +37,9 @@ class StateCollectingBackend(BaseDownloader):
 
 
 class MockStream(object):
-    def __init__(self, id):
+    def __init__(self, state_dict, id):
         self.id = id
+        self.state_dict = state_dict
 
     def is_valid(self):
         return True
@@ -59,10 +51,10 @@ class MockStream(object):
         return 'https://example.com/video/{}.mp4'.format(self.id)
 
     def create_downloader(self, backends):
-        return StateCollectingBackend(self.id)
+        return StateCollectingBackend(self.state_dict, self.id)
 
 
-def successful_clip(title='Test clip: S01E01-2018-07-01T00:00'):
+def successful_clip(state_dict, title='Test clip: S01E01-2018-07-01T00:00'):
     return Clip(
         webpage='https://areena.yle.fi/1-1234567',
         flavors=[
@@ -71,21 +63,21 @@ def successful_clip(title='Test clip: S01E01-2018-07-01T00:00'):
                 height=360,
                 width=640,
                 bitrate=880,
-                streams=[MockStream('1')]
+                streams=[MockStream(state_dict, '1')]
             ),
             StreamFlavor(
                 media_type='video',
                 height=720,
                 width=1280,
                 bitrate=1412,
-                streams=[MockStream('2')]
+                streams=[MockStream(state_dict, '2')]
             ),
             StreamFlavor(
                 media_type='video',
                 height=1080,
                 width=1920,
                 bitrate=2808,
-                streams=[MockStream('3')]
+                streams=[MockStream(state_dict, '3')]
             )
         ],
         title=title,
@@ -113,8 +105,6 @@ class DownloaderParametersFixture(object):
 
 @pytest.fixture
 def simple():
-    clear_state()
-
     return DownloaderParametersFixture(
         io=IOContext(destdir='/tmp/'),
         filters=StreamFilters(),
@@ -123,30 +113,29 @@ def simple():
 
 
 def test_download_success(simple):
-    global download_state
-
-    clips = [successful_clip()]
+    state = {}
+    clips = [successful_clip(state)]
     res = simple.downloader.download_episodes(
         clips, simple.io, simple.filters, None)
 
     assert res == RD_SUCCESS
-    assert download_state['command'] == 'download'
-    assert download_state['stream_id'] == '1'
+    assert state['command'] == 'download'
+    assert state['stream_id'] == '1'
 
 
 def test_pipe_success(simple):
-    global download_state
-
-    clips = [successful_clip()]
+    state = {}
+    clips = [successful_clip(state)]
     res = simple.downloader.pipe(clips, simple.io, simple.filters)
 
     assert res == RD_SUCCESS
-    assert download_state['command'] == 'pipe'
-    assert download_state['stream_id'] == '1'
+    assert state['command'] == 'pipe'
+    assert state['stream_id'] == '1'
 
 
 def test_print_urls(simple):
-    clips = [successful_clip()]
+    state = {}
+    clips = [successful_clip(state)]
 
     res = None
     with Capturing() as output:
@@ -154,11 +143,12 @@ def test_print_urls(simple):
 
     assert res == RD_SUCCESS
     assert output == ['https://example.com/video/1.mp4']
+    assert 'command' not in state
 
 
 def test_print_titles(simple):
     titles = ['Uutiset', 'Pasila: S01E01-2018-07-01T00:00']
-    clips = [successful_clip(t) for t in titles]
+    clips = [successful_clip({}, t) for t in titles]
 
     res = None
     with Capturing() as output:
@@ -169,12 +159,8 @@ def test_print_titles(simple):
 
 
 def test_download_failed_clip(simple):
-    global download_state
-
     clips = [failed_clip()]
     res = simple.downloader.download_episodes(
         clips, simple.io, simple.filters, None)
 
     assert res == RD_FAILED
-    assert download_state['command'] == None
-    assert download_state['stream_id'] == None
