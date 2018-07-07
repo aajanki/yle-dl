abe8258c2e039e11a4f4dc491b5f32b34760edff
Antti Ajanki <antti.ajanki@iki.fi>
2018-07-07 10:09:47 +0300

    Rename to avoid a name conflict

--- yledl/__init__.py
+++ yledl/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import print_function, absolute_import, unicode_literals
 from .yledl import download, StreamAction
-from .download import YleDlDownloader
+from .downloader import YleDlDownloader
 from .extractors import Clip, FailedClip, StreamFlavor, Subtitle
 from .downloaders import StreamFilters, IOContext, DownloadLimits, \
     BackendFactory
