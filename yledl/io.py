2561d30966b8c84b7116c6a30be2b71065eca7ee
Antti Ajanki <antti.ajanki@iki.fi>
2018-07-09 12:14:02 +0300

    Convert None to the default value

--- yledl/io.py
+++ yledl/io.py
@@ -16,14 +16,17 @@ class IOContext(object):
     outputfilename = attr.ib(default=None)
     destdir = attr.ib(default=None)
     resume = attr.ib(default=False)
-    download_limits = attr.ib(default=attr.Factory(
-        lambda: DownloadLimits()))
+    download_limits = attr.ib(
+        default=None,
+        converter=lambda x: x or DownloadLimits()
+    )
     excludechars = attr.ib(default='*/|')
     proxy = attr.ib(default=None)
     rtmpdump_binary = attr.ib(default=None)
-    hds_binary = attr.ib(default=attr.Factory(
-        lambda: ['php', resource_filename(__name__, 'AdobeHDS.php')]
-    ))
+    hds_binary = attr.ib(
+        default=None,
+        converter=lambda x: x or ['php', resource_filename(__name__, 'AdobeHDS.php')]
+    )
     ffmpeg_binary = attr.ib(default='ffmpeg')
     ffprobe_binary = attr.ib(default='ffprobe')
     wget_binary = attr.ib(default='wget')
