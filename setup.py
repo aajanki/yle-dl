3be3ebde227e4d470c16b0a47e57187799f84737
Antti Ajanki <antti.ajanki@iki.fi>
2018-10-13 16:36:11 +0300

    Attempt to get Travis to run the tests on Python 3.7

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet',
         'Topic :: Multimedia :: Video'
     ]
