2d8c2106c0368b8cb8e5053ac0626cc4b71833cd
Antti Ajanki <antti.ajanki@iki.fi>
2018-12-02 17:53:47 +0200

    Fix a test that was broken because of modified source data in Areena

--- tests/integration/test_areena_it.py
+++ tests/integration/test_areena_it.py
@@ -160,7 +160,7 @@ def test_areena_html5_clip_stream_url():
 
 
 def test_areena_episode_pages():
-    episodes = fetch_episode_pages('https://areena.yle.fi/1-3822119')
+    episodes = fetch_episode_pages('https://areena.yle.fi/1-3148871')
 
     # The first page contains 12 episodes, make sure we get several pages
     assert len(episodes) > 20
