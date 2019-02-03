54a351b55abb68c99e2c8a9c64618b83f9e23b40
Antti Ajanki <antti.ajanki@iki.fi>
2019-02-03 12:13:37 +0200

    wget is an optional dependency

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ yle-dl is a tool for downloading media files from the video streaming
 services of the Finnish national broadcasting company Yle: Yle
 Areena, Elävä Arkisto and Yle news.
 
-External dependencies: wget, ffmpeg. Certain streams also require:
-rtmpdump, and a PHP interpreter with the following PHP extensions:
+External dependencies: ffmpeg. Certain streams also require:
+rtmpdump, wget, or a PHP interpreter with the following PHP extensions:
 bcmath, curl, openssl and SimpleXML."""
 
 version = re.\
