9b06728ca89c59823b436330482ce0de3d6e9feb
Antti Ajanki <antti.ajanki@iki.fi>
2019-10-05 15:15:18 +0300

    HLS: Check if the stream has already been downloaded fully

--- yledl/streamprobe.py
+++ yledl/streamprobe.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function, absolute_import, unicode_literals
-import json
 import logging
-import subprocess
 from .backends import HLSBackend
+from .ffprobe import Ffprobe
 from .streamflavor import StreamFlavor, FailedFlavor
 
 
@@ -41,31 +40,3 @@ class FullHDFlavorProber(object):
             ))
 
         return sorted(res, key=lambda x: x.height)
-
-
-class Ffprobe(object):
-    def __init__(self, ffprobe_binary):
-        self.ffprobe_binary = ffprobe_binary
-
-    def show_programs_for_url(self, url):
-        debug = logger.isEnabledFor(logging.DEBUG)
-        loglevel = 'info' if debug else 'error'
-        args = [self.ffprobe_binary, '-v', loglevel, '-show_programs',
-                '-print_format', 'json=c=1', '-strict', 'experimental',
-                '-probesize', '80000000', '-i', url]
-        try:
-            return json.loads(subprocess.check_output(args))
-        except subprocess.CalledProcessError as ex:
-            raise ValueError(
-                f'Stream probing failed with status {ex.returncode}')
-
-    def duration_seconds_file(self, filename):
-        args = [self.ffprobe_binary, '-v', 'error', '-show_entries',
-                'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1',
-                filename]
-
-        try:
-            return float(subprocess.check_output(args))
-        except subprocess.CalledProcessError as ex:
-            raise ValueError(
-                f'Stream probing failed with status {ex.returncode}')
