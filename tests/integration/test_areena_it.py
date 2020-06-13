8e6e8c0a66f3065c22a38e4ed82feaaa1256d2f1
Antti Ajanki <antti.ajanki@iki.fi>
2020-06-13 18:24:47 +0300

    Fix tests
    Areena has provides more stream variants for some test cases

--- tests/integration/test_areena_it.py
+++ tests/integration/test_areena_it.py
@@ -19,7 +19,7 @@ def test_areena_html5_metadata():
 
     assert len(metadata) == 1
     flavors = metadata[0]['flavors']
-    assert len(flavors) == 2
+    assert len(flavors) == 5
     assert all(f.get('media_type') == 'video' for f in flavors)
     assert all('bitrate' in f and
                'height' in f and
@@ -53,7 +53,7 @@ def test_areena_iphone_stream_url():
     streamurl = fetch_stream_url('https://areena.yle.fi/1-4247408')
 
     assert len(streamurl) == 1
-    assert '/a.m3u8?' in streamurl[0]
+    assert '/a.mp4?' in streamurl[0]
 
 
 @pytest.mark.skipif(os.getenv('ENABLE_FINLAND_TESTS') != '1',
@@ -63,7 +63,7 @@ def test_areena_iphone_metadata():
 
     assert len(metadata) == 1
     flavors = metadata[0]['flavors']
-    assert len(flavors) == 1
+    assert len(flavors) == 6
     assert all(f.get('media_type') == 'video' for f in flavors)
     assert all('bitrate' in f and
                'height' in f and
