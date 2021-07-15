2c2aa944ef3981d639f1576d6a841cab6b595dd5
Pekka Ristola <pekkarr@protonmail.com>
2021-07-15 22:16:35 +0300

    Add HTTP proxy support to HLS backend

--- yledl/backends.py
+++ yledl/backends.py
@@ -249,7 +249,10 @@ class HLSBackend(ExternalDownloader):
         self.long_probe = long_probe
         self.program_id = program_id
         self.live = is_live
-        self.io_capabilities = frozenset([IOCapability.SLICE])
+        self.io_capabilities = frozenset([
+            IOCapability.SLICE,
+            IOCapability.PROXY
+        ])
         self.name = Backends.FFMPEG
 
     def file_extension(self, preferred):
@@ -259,6 +262,12 @@ class HLSBackend(ExternalDownloader):
     def _duration_arg(self, download_limits):
         if download_limits.duration:
             return ['-t', str(download_limits.duration)]
+        else:
+            return []
+
+    def _proxy_arg(self, io):
+        if io.proxy:
+            return ['-http_proxy', io.proxy]
         else:
             return []
 
@@ -348,6 +357,7 @@ class HLSBackend(ExternalDownloader):
             args.append('-stats')
         args.extend(self._probe_args())
         args.extend(self._seek_position_arg(io.download_limits))
+        args.extend(self._proxy_arg(io))
         args.extend(['-i', self.url])
         args.extend(self._duration_arg(io.download_limits))
         args.extend(self._metadata_args(clip, io))
