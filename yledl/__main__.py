402d3b27863aa80a67f5d71566eacc85ad9d7945
Antti Ajanki <antti.ajanki@iki.fi>
2022-08-04 19:36:12 +0300

    Add a license header on all source code files

--- yledl/__main__.py
+++ yledl/__main__.py
@@ -1,3 +1,20 @@
+# This file is part of yle-dl.
+#
+# Copyright 2010-2022 Antti Ajanki and others
+#
+# Yle-dl is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Yle-dl is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with yle-dl. If not, see <https://www.gnu.org/licenses/>.
+
 import sys
 
 from .yledl import main
