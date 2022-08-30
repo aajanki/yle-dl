b25b505fe1ed9e7eafc93d5792e7c73a54240a90
Antti Ajanki <antti.ajanki@iki.fi>
2022-08-30 19:40:21 +0300

    Bump version

--- yledl/version.py
+++ yledl/version.py
@@ -15,4 +15,4 @@
 # You should have received a copy of the GNU General Public License
 # along with yle-dl. If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = '20220704'
+__version__ = '20220830'
