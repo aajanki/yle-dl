f6548873d6ea81c61d4bde79acd35bad094c0df8
Antti Ajanki <antti.ajanki@iki.fi>
2024-04-28 21:15:20 +0300

    undefined variable

--- yledl/extractors.py
+++ yledl/extractors.py
@@ -1000,17 +1000,16 @@ class AreenaLiveTVExtractor(AreenaExtractor):
         return [url]
 
     def extract_clip(self, clip_url, origin_url):
+        pageurl = 'https://areena.yle.fi/'
         pids = {
             'tv1': 'yle-tv1',
             'tv2': 'yle-tv2',
             'teema': 'yle-teema-fem',
         }
-
         pid = pids.get(clip_url.lower())
         if not pid:
-            return FailedClip(url, f'"{clip_url}" is not a live TV channel')
+            return FailedClip(pageurl, f'"{clip_url}" is not a live TV channel')
 
-        pageurl = 'https://areena.yle.fi/'
         program_info = self.program_info_for_pid(
             pid, pageurl, self.title_formatter, self.ffprobe)
         return self.create_clip_or_failure(pid, program_info, clip_url, origin_url)
