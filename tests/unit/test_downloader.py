f8f2ab550078a096ff2b3da35aa08e66af100b92
evidencebp <idanamit@gmail.com>
2024-11-04 07:11:21 +0200

    Revert "Import Clip from new location - WIP"
    This reverts commit 4bff9d6e9007745d06644d96387c8d21a8508aff.

--- tests/unit/test_downloader.py
+++ tests/unit/test_downloader.py
@@ -28,8 +28,7 @@ from yledl import StreamFilters, IOContext, RD_SUCCESS, RD_FAILED
 from yledl.backends import BaseDownloader, FailingBackend
 from yledl.downloader import YleDlDownloader
 from yledl.errors import TransientDownloadError
-from yledl.data_extractors import Clip
-from yledl.extractors import FailedClip, StreamFlavor
+from yledl.extractors import Clip, FailedClip, StreamFlavor
 from yledl.titleformatter import TitleFormatter
 
 
