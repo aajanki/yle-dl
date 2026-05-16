94e55e897c4454a1da0446653d8a06d8faed061d
Antti Ajanki <antti.ajanki@iki.fi>
2026-05-16 17:36:21 +0300

    Use a mocked Clip on tests

--- tests/unit/test_backend.py
+++ tests/unit/test_backend.py
@@ -1,6 +1,6 @@
 # This file is part of yle-dl.
 #
-# Copyright 2010-2025 Antti Ajanki and others
+# Copyright 2010-2026 Antti Ajanki and others
 #
 # Yle-dl is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
@@ -15,13 +15,23 @@
 # You should have received a copy of the GNU General Public License
 # along with yle-dl. If not, see <https://www.gnu.org/licenses/>.
 
+from datetime import datetime
 from yledl import RD_SUCCESS
 from yledl.backends import DASHHLSBackend, WgetBackend
-from utils import MockIOContext
-
+from yledl.clip import Clip
+from utils import FixedOffset, MockIOContext
 
 tv1_url = 'https://yletv-lh.akamaihd.net/i/yletv1hls_1@103188/master.m3u8'
 io = MockIOContext(destdir='/tmp/')
+mock_clip = Clip(
+    webpage='https://areena.yle.fi/1-1234567',
+    flavors=[],
+    title='Test clip: S01E01-2018-07-01T00:00',
+    duration_seconds=950,
+    region='Finland',
+    publish_timestamp=datetime(2018, 7, 1, tzinfo=FixedOffset(3)),
+    expiration_timestamp=datetime(2019, 1, 1, tzinfo=FixedOffset(3)),
+)
 
 
 class MockHLSBackend(DASHHLSBackend):
@@ -55,7 +65,7 @@ class MockWgetBackend(WgetBackend):
 def test_hls_backend_save_stream():
     backend = MockHLSBackend(tv1_url, program_id=0)
 
-    res = backend.save_stream('test.mkv', clip=None, io=io)
+    res = backend.save_stream('test.mkv', clip=mock_clip, io=io)
 
     assert res == RD_SUCCESS
     assert len(backend.executed_commands) == 1
@@ -75,7 +85,7 @@ def test_hls_backend_pipe():
 def test_wget_backend_save_stream():
     backend = MockWgetBackend(tv1_url, file_extension='.mkv')
 
-    res = backend.save_stream('test.mkv', clip=None, io=io)
+    res = backend.save_stream('test.mkv', clip=mock_clip, io=io)
 
     assert res == RD_SUCCESS
     assert len(backend.executed_commands) == 1
